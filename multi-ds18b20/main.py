4323a7eeed1fce3e722b489cd47af629005906fc
zzZ5 <baoju_liu@foxmail.com>
2021-07-21 17:30:53 +0800

    重构测试代码

--- multi-ds18b20/main.py
+++ multi-ds18b20/main.py
@@ -7,7 +7,7 @@ import machine
 
 ow = onewire.OneWire(machine.Pin(4))  # 创建onewire总线 引脚4（G4）
 ds = ds18x20.DS18X20(ow)
-time_interval = 6000
+sleep_time = 5000
 wlan = network.WLAN(network.STA_IF)  # 设置开发板的网络模式
 wlan.active(True)
 
@@ -15,23 +15,22 @@ wlan.active(True)
 def GetTemp():
     roms = ds.scan()  # 扫描总线上的设备
     ds.convert_temp()  # 获取采样温度
-
-    return ds.read_temp(roms[0]), ds.read_temp(roms[1])  # 得到温度
+    for i in roms:
+        yield ds.read_temp(i)
 
 
 def do_measure():
-    temp_ = GetTemp()  # 读取measure()函数中的温度数据
-    print(temp_)
+    print("temperature:")
+    for temp in GetTemp():
+        print(temp, end='  ')
+    print("")
+    print("====================")
 
 
 while True:
     try:
-        t1 = utime.ticks_ms()
         do_measure()
-        t2 = utime.ticks_ms()
-        sleep_time = time_interval - (t2 - t1)
-        if sleep_time > 0:
-            utime.sleep_ms(sleep_time)
+        utime.sleep_ms(sleep_time)
     except Exception as e:
         print(e)
         utime.sleep(1)
