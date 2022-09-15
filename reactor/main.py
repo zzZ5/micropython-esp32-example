5ce88db49cdcc0a84c2fcfade2e6c861ac828bff
zzZ5 <baoju_liu@foxmail.com>
2022-09-15 17:38:38 +0800

    添加CO2传感器

--- reactor/main.py
+++ reactor/main.py
@@ -1,3 +1,4 @@
+import adafruit_sgp30
 from mqtt import MQTTClient
 
 import ds18x20
@@ -6,7 +7,7 @@ import onewire
 import uasyncio as asyncio
 import ujson as json
 import utime as time
-from machine import Pin
+from machine import I2C, Pin
 
 # 参数设置
 config = {
@@ -25,9 +26,19 @@ ntp_host = []
 ntp_interval = 1000
 temp_maxdif = 5
 
+# 温度传感器
 ow = onewire.OneWire(machine.Pin(4))  # 创建onewire总线 引脚4（G4）
-ds = ds18x20.DS18X20(ow)
+ds = ds18x20.DS18X20(ow)  # 创建ds18b20传感器
 
+
+# 二氧化碳传感器
+i2c = I2C(0)
+i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=100000)
+sgp30 = adafruit_sgp30.Adafruit_SGP30(i2c)  # 创建sgp30传感器 引脚22、21（G22、G21）
+baseline_time = time.time()
+
+# 继电器
+pin2 = Pin(2, Pin.OUT, value=1)
 heat = True  # 当前是否正在加热
 
 
@@ -52,6 +63,8 @@ def read_config():
         ntp_host = config['ntp_host']
         ntp_interval = config['ntp_interval']
         temp_maxdif = config['temp_maxdif']
+
+    print("配置文件提取完成！")
 
 
 def update_config(new_config, restart=False):
@@ -80,6 +93,7 @@ def sync_ntp():
     通过网络校准时间。
     '''
 
+    print("开始校准时间......")
     import ntptime
     ntptime.NTP_DELTA = 3155644800  # 可选 UTC+8偏移时间（秒），不设置就是UTC0
     is_setted = False
@@ -99,6 +113,7 @@ def sync_ntp():
                     machine.reset()
                 times += 1
                 continue
+    print("时间校准完成！")
 
 
 def wlan_connect(ssid, password):
@@ -110,6 +125,7 @@ def wlan_connect(ssid, password):
         password: wifi密码。
     '''
 
+    print("开始连接网络......")
     import network
     wlan = network.WLAN(network.STA_IF)
     if not wlan.active() or not wlan.isconnected():
@@ -119,6 +135,18 @@ def wlan_connect(ssid, password):
         while not wlan.isconnected():
             pass
     print('network config:', wlan.ifconfig())
+    print("网络连接成功！")
+
+
+def init_sgp():
+    '''
+    初始化spg30传感器
+    '''
+    print("初始化spg30传感器......")
+    sgp30.iaq_init()
+    print("Waiting 15 seconds for SGP30 initialization.")
+    time.sleep(15)
+    print("spg30传感器初始化成功！")
 
 
 def get_temp():
@@ -126,28 +154,57 @@ def get_temp():
     获取传感器温度数据。
     扫描总线以及配置的keys，确保key和温度一一匹配。
     '''
+
     try:
         roms = ds.scan()  # 扫描总线上的设备
-        assert len(roms) == len(keys), 'The quantity does not match.'
+        assert len(roms) == len(keys["ds"]), 'The quantity does not match.'
         ds.convert_temp()  # 获取采样温度
-        for i, key in zip(roms, keys):
+        for i, key in zip(roms, keys["ds"]):
             yield ds.read_temp(i), key
     except:
         time.sleep_ms(1)
         machine.reset()
 
 
+def get_CO2():
+    '''
+    获取sgp30传感器的CO2数据。
+    '''
+    global baseline_time
+    co2eq, tvoc = sgp30.iaq_measure()
+
+    if (time.time() - baseline_time >= 3600):
+        print('Saving baseline!')
+
+        baseline_time = time.time()
+
+        try:
+            f_co2 = open('co2eq_baseline.txt', 'w')
+            f_tvoc = open('tvoc_baseline.txt', 'w')
+
+            bl_co2, bl_tvoc = sgp30.get_iaq_baseline()
+            f_co2.write(str(bl_co2))
+            f_tvoc.write(str(bl_tvoc))
+
+            f_co2.close()
+            f_tvoc.close()
+
+        except:
+            print('Impossible to write SGP30 baselines!')
+
+    return co2eq, keys['sgp']
+
+
 def control_heat(cmd):
     global heat
-    pin2 = Pin(2, Pin.OUT, value=1)
     if cmd == True:
         pin2.on()
         heat = True
-        print('heater on')
+        # print('heater on')
     else:
         pin2.off()
         heat = False
-        print('heater off')
+        # print('heater off')
 
 
 class MyIotPrj:
@@ -217,13 +274,13 @@ class MyIotPrj:
             if conn_ret_code != 0:
                 return
 
+            print("开始连接MQTT服务器......")
             print('conn_ret_code = {0}'.format(conn_ret_code))
-
             await self.client.subscribe(self.topic_ctl)
             print("Connected to {}, subscribed to {} topic".format(
                 self.mserver, self.topic_ctl))
-
             self.isconn = True
+            print("MQTT服务器连接成功！开始监听数据......")
 
             while True:
                 await self.client.wait_msg()
@@ -246,21 +303,35 @@ class MyIotPrj:
             t1 = time.ticks_ms()
             if self.isconn == True:
                 datas = {"data": []}
-                for temp, key in get_temp():
-                    if temp in value_skip:
+                # 添加温度数据
+                for temp_value, temp_key in get_temp():
+                    if temp_value in value_skip:
                         continue
-                    data = {"value": temp,
-                            "key": key,
-                            "measured_time": "{}-{}-{} {}:{}:{}".format(*time.localtime())}
-                    datas["data"].append(data)
+                    data_temp = {"value": temp_value,
+                                 "key": temp_key,
+                                 "measured_time": "{}-{}-{} {}:{}:{}".format(*time.localtime())}
+                    datas["data"].append(data_temp)
+
+                # 控制加热开关
                 data_inside, data_outside = split_temp(datas["data"])
                 if compare(data_inside, data_outside):
                     control_heat(True)
                 else:
                     control_heat(False)
                 datas["info"] = {"heat": heat}
-                print(datas["data"])
+
+                # 添加CO2数据 ps:顺序一定不能错，先添加温度数据，再控制开关，最后添加CO2数据
+                co2_value, co2_key = get_CO2()
+                if co2_value not in value_skip:
+                    data_co2 = {"value": co2_value,
+                                "key": co2_key,
+                                "measured_time": "{}-{}-{} {}:{}:{}".format(*time.localtime())}
+                    datas["data"].append(data_co2)
+
+                # print("上传数据：")
+                # print(datas["data"])
                 await self.client.publish(self.topic_sta.format(equipment_key, 'post', 'data').encode(), json.dumps(datas), retain=False)
+
             t2 = time.ticks_ms()
             sleep_time = post_interval * 1000 - (t2 - t1)
             await asyncio.sleep_ms(sleep_time)
@@ -294,7 +365,7 @@ def median(data):
 def compare(temp1, temp2):
     temp_dif = median(temp1) - median(temp2)
     # temp_dif = -temp_dif if temp_dif < 0 else temp_dif
-    print("temp1: {} temp2: {}, dif: {}".format(temp1, temp2, temp_dif))
+    # print("temp1: {} temp2: {}, dif: {}".format(temp1, temp2, temp_dif))
     if temp_dif >= temp_maxdif:
         return 1
     else:
@@ -305,6 +376,7 @@ def main():
     read_config()
     wlan_connect(wifi_name, wifi_password)
     sync_ntp()
+    init_sgp()
     mip = MyIotPrj()
     loop = asyncio.get_event_loop()
 
